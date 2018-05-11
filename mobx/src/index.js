61c0166ee657935e32d321327c79f9280b27da09
Andrei Pfeiffer <andrei@upsidedown.ro>
2018-05-11 14:34:52 +0300

    Update mobx example

--- mobx/src/index.js
+++ mobx/src/index.js
@@ -5,8 +5,8 @@ import { Provider } from "mobx-react";
 import "./assets/index.css";
 import App from "./App";
 
-// import stores from "./store-mobx";
-import stores from "./store-mobx-state-tree";
+import stores from "./store-mobx";
+// import stores from "./store-mobx-state-tree";
 
 ReactDOM.render(
   <Provider {...stores}>
